c6a967d4e7b80c0743ac1a6cda96e7957825f9b9
Jerry Wang <doghost111@gmail.com>
2023-08-31 21:50:41 +0800

    Feat : 特色頁面 RWD

--- src/views/CharacteristicView.vue
+++ src/views/CharacteristicView.vue
@@ -1,5 +1,44 @@
 <template>
-  <div>charcteristic</div>
+  <div class="character-container">
+    <div class="charcter-header">
+      <div class="charcter-header-title">白頭翁(Chinese bulbul)</div>
+      <div class="charcter-header-content">
+        又名白頭鵯。以果實、昆蟲無主食，無法消化小米、穀類。平均壽命約 8~10 年。
+      </div>
+    </div>
+    <div class="charcter-content-container">
+      <div class="charcter-content">
+        <div class="charcter-content-title">
+          <div class="charcter-content-title-text">外觀</div>
+        </div>
+        <div class="charcter-content-content">
+          白頭鴇體長約17到22公分,額 至頭頂純黑色而富有光澤,兩
+          眼上方至後枕白色,形成一白 色枕環。耳羽後部有一白班,
+          此白環與白斑在黑色的頭部均 極為醒目,老烏的枕羽(後頭部)
+          更潔白,所以又叫「白頭翁」。
+        </div>
+      </div>
+      <div class="charcter-content">
+        <div class="charcter-content-title">
+          <div class="charcter-content-title-text">棲地</div>
+        </div>
+        <div class="charcter-content-content">
+          白頭翁和麻雀、綠繡眼合 稱「城市三寳」,常成群出現
+          在平原區灌木叢、丘陵樹林地 帶,以及校園、公園、庭院、
+          行道中的各種高高的電線與樹 上。
+        </div>
+      </div>
+      <div class="charcter-content">
+        <div class="charcter-content-title">
+          <div class="charcter-content-title-text">食性</div>
+        </div>
+        <div class="charcter-content-content">
+          以果樹的漿果和種子為主食, 並時常飛入果園偷吃果寞,還
+          會吃嫩葉嫩芽,尤其是胡蝶蘭 的嫩葉嫩芽葉,偶爾啄食昆 蟲。
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,4 +46,3 @@ export default {
   name: "CharacteristicView",
 };
 </script>
-
